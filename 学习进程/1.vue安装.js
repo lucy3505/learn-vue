c151ec76c826b98ed3a426fd215e264a741aad60
xuyanyu <13022112163@163.com>
2019-12-23 14:59:49 +0800

    init

---
+++ 学习进程/1.vue安装.js
@@ -0,0 +1,17 @@
+/*
+
+``vscode 2.0安装
+$ vue init webpack vue-demo
+
+? Project name learn-vue
+? Project description A Vue.js project
+? Author xuyanyu <13022112163@163.com>
+? Vue build standalone
+? Install vue-router? Yes
+? Use ESLint to lint your code? Yes
+? Pick an ESLint preset Standard
+? Set up unit tests No
+? Setup e2e tests with Nightwatch? No
+? Should we run `npm install` for you after the project has been created? (recommended) npm
+
+*/
