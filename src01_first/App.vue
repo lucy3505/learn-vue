7b2b62e3417e3b104573a5e2a09d325235f2b641
xuyanyu <13022112163@163.com>
2019-12-24 18:10:54 +0800

    change

---
+++ src01_first/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <hello-world />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+export default {
+  name: "App",
+  components: { HelloWorld }
+};
+</script>
+
+<style>
+#app {
+}
+</style>
