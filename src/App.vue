210b966418ef4b0dce03e29c191e5e6d96538219
lucy3505 <415492767@qq.com>
2019-12-29 15:51:05 +0800

    'change'

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,56 @@
 <template>
-    <div class="todo-container">
-      <Header :addTodo="addTodo"/>
-      <List :todos="todos" :deleteTodo="deleteTodo" :updateComplete="updateComplete" />
-      <Footer :todos="todos" :handleAllComplete="handleAllComplete" :clearAllComplete="clearAllComplete"/>
-    </div>
+  <div class="todo-container">
+    <Header :addTodo="addTodo" />
+    <List :todos="todos" :deleteTodo="deleteTodo" :updateComplete="updateComplete" />
+    <Footer
+      :todos="todos"
+      :handleAllComplete="handleAllComplete"
+      :clearAllComplete="clearAllComplete"
+    />
+  </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import List from './components/List.vue'
-import Footer from './components/Footer.vue'
-import './base.css'
-  export default {
-    components:{Header,List,Footer},
-    data() {
-        return {
-          todos:[{
-            title:'AAA',
-            complete:false,
-            id:1
-          },
-          {
-            title:'BBB',
-            complete:true,
-            id:2
-          },
-          {
-            title:'CCC',
-            complete:false,
-            id:3
-          },
-          {
-            title:'DDD',
-            complete:false,
-            id:4
-          },]
-        };
-    },
-    methods:{
-      addTodo(value){
-        this.todos.unshift(value)
-      },
-      deleteTodo(index){
-        this.todos.splice(index,1)
-      },
-      updateComplete(todo,isComplete){
-        this.todos.forEach(item=>{
-          if(item.id===todo.id){
-            item.complete = isComplete
-          }
-        })
-      },
-      handleAllComplete(isCheck){
-        // debugger
-        this.todos.forEach((item)=>item.complete=isCheck)
-      },
-      clearAllComplete(){
-        this.todos = this.todos.filter((item)=>
-          !item.complete
-        )
+import Header from "./components/Header.vue";
+import List from "./components/List.vue";
+import Footer from "./components/Footer.vue";
+import "./base.css";
+export default {
+  components: { Header, List, Footer },
+  data() {
+    return {
+      todos: JSON.parse(localStorage.getItem("todos_key") || "[]")
+    };
+  },
+  watch: {
+    todos: {
+      deep: true,
+      handler: function(value) {
+        localStorage.setItem("todos_key", JSON.stringify(value));
       }
     }
- };
+  },
+  methods: {
+    addTodo(value) {
+      this.todos.unshift(value);
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    updateComplete(todo, isComplete) {
+      todo.complete = isComplete;
+    },
+    handleAllComplete(isCheck) {
+      // debugger
+      this.todos.forEach(item => (item.complete = isCheck));
+    },
+    clearAllComplete() {
+      this.todos = this.todos.filter(item => !item.complete);
+    }
+  }
+};
 </script>
 
 <style scoped >
-
 </style>
 
